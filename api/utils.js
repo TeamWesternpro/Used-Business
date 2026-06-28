264214e0829856251d989539ff8df428ae9618f8
Team Western <michael.g.p.westernil@gmail.com>
2026-06-28 18:17:59 +0100

    Update utils.js

--- api/utils.js
+++ api/utils.js
@@ -4,6 +4,7 @@ const path = require('path');
 const DATA_DIR = '/tmp/usedbusiness';
 const LISTINGS_FILE = path.join(DATA_DIR, 'listings.json');
 const CONFIG_FILE = path.join(DATA_DIR, 'config.json');
+const BOOKINGS_FILE = path.join(DATA_DIR, 'bookings.json');
 
 function ensureDir() {
     try { fs.mkdirSync(DATA_DIR, { recursive: true }); } catch (e) {}
@@ -44,6 +45,14 @@ function saveConfig(config) {
     saveJSON(CONFIG_FILE, config);
 }
 
+function getBookings() {
+    return readJSON(BOOKINGS_FILE, []);
+}
+
+function saveBookings(bookings) {
+    saveJSON(BOOKINGS_FILE, bookings);
+}
+
 function generateId() {
     return Date.now().toString() + Math.floor(Math.random() * 9000 + 1000).toString();
 }
@@ -68,4 +77,4 @@ function sendJSON(res, status, data) {
     res.end(JSON.stringify(data));
 }
 
-module.exports = { getListings, saveListings, getConfig, saveConfig, generateId, readBody, sendJSON };
+module.exports = { getListings, saveListings, getConfig, saveConfig, getBookings, saveBookings, generateId, readBody, sendJSON };
